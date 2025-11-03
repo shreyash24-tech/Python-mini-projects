f26ab3e115c24c4b1fd46f4755ae1a9cf107892c
shreyash24-tech <shreyashdhawale63@gmail.com>
2025-11-03 20:02:33 +0530

    Add files via upload

---
+++ Basic-Level/Largest_among_3.py
@@ -0,0 +1,11 @@
+n1=int(input("Enter 1st Number="))
+n2=int(input("Enter  2nd Number="))
+n3=int(input("Enter 3rd number="))
+
+if(n1>n2 and n1>n3):
+    print("Largest Number is=",n1)
+elif(n2>n3 and n2>n1):
+    print("Largest number is ",n2)
+else:
+    print("Largest numer is ",n3)
+  
